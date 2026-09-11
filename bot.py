7efcfcdf6a12e4f356fae8cc94bc4bef5862922a
t1mwatson-bel <mix3005@yandex.ru>
2026-09-12 02:41:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -673,11 +673,11 @@ def get_unit_prediction(game):
     и третья карта Player не являются условиями.
     """
 
-    player = game.get("player_cards", [])
+        player = game.get("player_cards", [])
     dealer = game.get("dealer_cards", [])
 
-if len(player) < 1 or len(dealer) < 1:
-    return None
+    if len(player) < 1 or len(dealer) < 1:
+        return None
 
     # Если в триггерной игре у Player ИЛИ у Dealer 2 карты —
     # прогноз не создаём.
